28fc3907e7871545300b4890003688bad1604b34
Garret Gallant <gallanttechnological@gmail.com>
2020-09-01 19:21:08 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -17,7 +17,7 @@ def find_min_in_nested_arrays(src)
   while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
-      number = 0
+      number = 99
       if number.to_i < src[row_index][element_index]
           src[row_index][element_index] = number
       end
